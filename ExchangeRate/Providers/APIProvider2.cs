17e58366df6cf0c4aafe70ee6bfdaca621a92ebc
ismaeldcent04 <ismadadi.26@gmail.com>
2025-08-04 07:21:18 -0400

    Creacion de proveedores e interfaz

---
+++ ExchangeRate/Providers/APIProvider2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Providers
+{
+    public class APIProvider2: IProvider
+    {
+        public void Convert() 
+        {
+
+        }
+    }
+}
---
+++ ExchangeRate/Providers/IProvider.cs
@@ -0,0 +1,14 @@
+﻿using ExchangeRate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRate.Providers
+{
+    public interface IProvider
+    {
+       void Convert();
+    }
+}
