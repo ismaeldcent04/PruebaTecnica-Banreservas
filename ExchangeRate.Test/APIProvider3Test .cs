dfd6d2cf50d3eb97fdf0bf1fecb7d2a44c4c5531
ismaeldcent04 <ismadadi.26@gmail.com>
2025-08-04 21:13:17 -0400

    .

---
+++ ExchangeRate.Test/APIProvider3Test .cs
@@ -0,0 +1,24 @@
+﻿using ExchangeRate.Models;
+using ExchangeRate.Providers;
+
+namespace ExchangeRate.Test
+{
+    [TestClass]
+    public sealed class APIProviderTest3
+    {
+        [TestMethod]
+        public async Task Convert_ReturnsSuccessfulResult()
+        {
+
+            var provider = new APIProvider3();
+
+            var request = new ExchangeRequest("USD", "DOP", 100);
+
+            var result = await provider.Convert(request);
+
+            Assert.IsTrue(result.IsSuccessful);
+            Assert.AreEqual("fastforex", result.Provider);
+            Assert.IsTrue(result.ExchangeRate > 0);
+        }
+    }
+}
